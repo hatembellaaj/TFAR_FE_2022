191522f7bdde5d28b63d122f29aa3a2d47bb5868
hatem.bellaaj <hatem.bellaaj@magnolia.fr>
2022-04-28 22:20:42 +0100

    it works with security : it's great :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,27 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MaterialModule } from './material/material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NgChartsModule } from 'ng2-charts';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { authInterceptorProviders } from './_helpers/auth.interceptor';
+
+
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent    
   ],
   imports: [
     BrowserModule,
@@ -18,11 +36,12 @@ import { NgChartsModule } from 'ng2-charts';
     NgChartsModule,
     LayoutModule,
     MaterialModule,
+    FormsModule,
     HttpClientModule
 
   ],
   exports: [],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
   
